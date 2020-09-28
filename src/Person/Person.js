33709bd8c3c9db238791b3effdfef2170cf59222
Eduardo <eduardo.peracchia@gmail.com>
2020-09-29 00:54:58 +0200

    Test push

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>Hi, I'm {props.name} and I am {props.age} y/o!</p>
+    return <p>Hi, I'm {props.name}  and I am {props.age} y/o!</p>
 }
 
 export default person;
