0a5f77ef2381a8ff2debbbcaeb1167122d367f97
Edu <eduardo.peracchia@gmail.com>
2020-10-17 12:52:10 +0200

    Test Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <Person name="Edu" age="28"></Person>
-        <Person name="Cami" age="26"/>
+        <Person name="Cami" age="26"/> 
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
