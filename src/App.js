631e67b4fb38b4e6f6607d7db433a99687d4de29
Edu <eduardo.peracchia@gmail.com>
2020-11-07 17:16:32 +0100

    Ripresa lezione

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <Person name="Edu" age="28"></Person>
         <Person name="Cami" age="26"/> 
-      </div>
+      </div> 
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const person = (props) => {
+const person = (props) => { 
     return <p>Hi, I'm {props.name}  and I am {props.age} y/o!</p>
 }
 
