161dc89b0dbbcc81247b5008f36b54d165a4d3ee
Edu <eduardo.peracchia@gmail.com>
2020-11-22 13:02:23 +0100

    Lezione sugli Styled Components

--- src/App.js
+++ src/App.js
@@ -1,67 +1,114 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  boder: 1px solid red;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  } 
+`;
 
 class App extends Component {
 
   state = {
     persons : [
-      {name: "Edu", age: "28"}, 
-      {name: "Cami", age: "26"}
-    ]
+      {id: 'asd', name: "Edu", age: "28"}, 
+      {id: 'dsa', name: "Cami", age: "26"},
+      {id: 'fsa', name: "Franky", age: "28"}
+    ],
+    showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('clicked');
-    // WRONG! //this.state.persons[0].name = 'Eduardo';
-    this.setState({
-      persons : [
-        {name: newName, age: "28"}, 
-        {name: "Cami", age: "26"}
-      ]
-    })
+  
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    //non va bene così perchè vado a mutare l'oggetto principale
+    // const person = this.state.persons[personIndex];
+    // quindi uso lo spread operator ...
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    //copio array di persons usando lo spread operator sul mio array di persons in state
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons : persons });
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons : [
-        {name: event.target.value, age: "28"}, 
-        {name: "Cami", age: "26"}
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    //questo non è una best practice, perchè abbiamo un puntatore diretto a this.state.persons, che va a modificarne
+    //lo stato in modo permanente
+    //questi due metodi qui sotto per definire persons come copia di this.state.persons sono equivalenti
+    // e sono best practices in quanto non modificano this.state.persons 
+    //const persons = this.state.persons 
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   }
 
   render() {
 
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      boxshadow: '0 2px 3px #ccc',
-      boder: '1px solid red',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          {
+            this.state.persons.map((p,i)=>{
+              return <Person 
+                click={() =>this.deletePersonHandler(i)}
+                name={p.name} 
+                age={p.age}
+                key={p.id}
+                changed={(event) => this.nameChangeHandler(event, p.id)}/>
+            })
+          }
+        </div>
+      );
+
+
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <=1){
+      classes.push('bold');
+    }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <button 
-          style={style}
-          onClick={() => this.switchNameHandler("Eduardo!")}>
-            Switch name
-        </button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this, "Edu!")}
-          changed={this.nameChangeHandler}>
-            Questo posso farlo grazie a props.children
-        </Person>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[0].age}>
-        </Person> 
-      </div> 
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <StyledButton
+            alt={this.state.showPersons}
+            onClick={this.togglePersonsHandler}> Toggle Persons </StyledButton>
+          {persons}
+        </div> 
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
