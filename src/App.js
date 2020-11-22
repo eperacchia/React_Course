2a62345b35ad042e2d84986a0e5a4683fcc80506
Edu <eduardo.peracchia@gmail.com>
2020-11-22 13:11:03 +0100

    Rimosso styled componentss da app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  boder: 1px solid red;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  } 
-`;
 
 class App extends Component {
 
@@ -104,9 +89,10 @@ class App extends Component {
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
-          <StyledButton
-            alt={this.state.showPersons}
-            onClick={this.togglePersonsHandler}> Toggle Persons </StyledButton>
+          <button
+            className="button"
+            // alt={this.state.showPersons}
+            onClick={this.togglePersonsHandler}> Toggle Persons </button>
           {persons}
         </div> 
     );
